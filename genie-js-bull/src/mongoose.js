47867420cdcf7dc0c8475384b0a8e1c3a7773e72
geniein <cubya0104@gmail.com>
2021-04-04 23:06:35 +0900

    cm

--- genie-js-bull/src/mongoose.js
+++ genie-js-bull/src/mongoose.js
@@ -3,23 +3,14 @@ import CallApi from './models/callApi.model';
 
 mongoose.Promise = Promise;
 
-// Exit application on error
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
   process.exit(-1);
 });
 
-// print mongoose logs in dev env
 if (process.env.ENV === 'development') {
   mongoose.set('debug', true);
 }
-
-/**
- * Connect to mongo db
- *
- * @returns {object} Mongoose connection
- * @public
- */
 
 export const connectToMongo = () => {
   mongoose.connect(process.env.MONGO_URI, {
